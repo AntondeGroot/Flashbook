25c4171c581706cc187656b8bc8894cd739e3771
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2018-11-05 21:37:13 +0100

    Update gui_flashbook.py

--- files/gui_flashbook.py
+++ files/gui_flashbook.py
@@ -23,46 +23,7 @@ path_add = os.path.join(dir7,"add.png")
 path_min = os.path.join(dir7,"min.png")
 path_switch = os.path.join(dir7,"repeat.png")
 
-###########################################################################
-## Class MyFrame
-###########################################################################
 
-import wx
-import wx.xrc
-import wx.richtext
-
-###########################################################################
-## Class MyFrame
-###########################################################################
-# -*- coding: utf-8 -*- 
-
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
-import wx
-import wx.xrc
-import wx.richtext
-
-###########################################################################
-## Class MyFrame
-###########################################################################
-
-# -*- coding: utf-8 -*- 
-
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
-import wx
-import wx.xrc
-import wx.richtext
 
 ###########################################################################
 ## Class MyFrame
