805f077b0c49eafc15134d32d4ef1b294636c38a
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2018-12-20 17:09:05 +0100

    Update program.py

--- files/program.py
+++ files/program.py
@@ -263,8 +263,15 @@ def get_IP(self,event):
         self.IP2 = data['IP2']
     self.m_txtMyIP.SetValue(self.IP1)
     self.m_txtTargetIP.SetValue(self.IP2)
-
+import wmi
 def run_transfer(self,event,client):    
+    wmi_obj = wmi.WMI()
+    wmi_sql = "select IPAddress,DefaultIPGateway from Win32_NetworkAdapterConfiguration where IPEnabled = True"
+    wmi_out = wmi_obj.query(wmi_sql)
+    for dev in wmi_out:
+        print("IPv4Address:", dev.IPAddress[0], "DefaultIPGateway:", dev.DefaultIPGateway[0])
+    print("IPv4Address:\t", dev.IPAddress[0])
+    CurrentIP = dev.IPAddress[0]
     
     # both Client as Host use the same 'IP2' because IP2 is the IP address the device is trying to connect to.
     startdir = os.path.join(os.getenv("LOCALAPPDATA") ,"testtransfer")
@@ -275,10 +282,14 @@ def run_transfer(self,event,client):
             
         dirappendlist = ["books","pics","resources"] # directories that should not be overwritten but appended when new files are added
         HostIP = self.IP2
-        server.start_client(dirlist,dirappendlist,HostIP)        
+        #server.start_client(dirlist,dirappendlist,HostIP)    
+        t_client = lambda HOST,PORT,HostIP :threading.Thread(target=server.start_client,args=(dirlist,dirappendlist,HostIP) ).start()
+        t_client(dirlist,dirappendlist,HostIP) 
     else: #start server
         ClientIP = self.IP2
-        server.start_server(startdir,ClientIP)
+        #server.start_server(startdir,ClientIP)
+        t_server = lambda HOST,PORT,CurrentIP :threading.Thread(target=server.start_server,args=(startdir,ClientIP,CurrentIP)).start()
+        t_server(startdir,ClientIP,CurrentIP)
     
 def run_print(self,event):                
     
