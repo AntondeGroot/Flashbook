5d0240d36721ad618aaffcc5cce4da40dd7c9ad4
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-01-06 18:31:57 +0100

    Update pdf_modules.py

--- files/pdf_modules.py
+++ files/pdf_modules.py
@@ -22,30 +22,37 @@ def ConvertPDF_to_JPG(self):
     
     for i, item in enumerate(arr_pdf):
         pdfname = item[:-4]
-        print(pdfname)
+        print(pdfname, len(pdfname))
         
         origin_dir = os.path.join(self.dirpdfbook, item)
         target_dir = os.path.join(self.dir3, pdfname)
         
         if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
+            os.makedirs(target_dir)        
         
-        if os.listdir(target_dir) == []:
-            #The method convert_from_path will work for a lot of pdfs                 #
-            #Yet when this method fails convert_from_bytes might convert pdfs that    #
-            #couldn't be converted with from_path, if both methods don't work the user#
-            #is informed to use an online-converter instead.                          #
-            pages = convert_from_path(item, 170)
-            if pages == []:
-                pages = convert_from_bytes(open(origin_dir, 'rb').read())
-                
-            if pages != []:
-                ctypes.windll.user32.MessageBoxW(0, f'The file "{pdfname}.pdf" could not be converted to jpg.\nPlease use an online pdf converter instead.\nThen place the jpg files in a map folder named after the book in {self.dir3}', "Warning", 1)            
-                
-            if pages != []:
-                i = 1
-                for page in pages:
-                    filename = os.path.join(target_dir, pdfname)
-                    nr = "0"*(4-len(f"{i}"))+f"{i}"
-                    page.save(f'{filename}{nr}.jpg', 'JPEG')
-                    i += 1
+        try:
+            os.listdir(target_dir)
+        except:
+            ctypes.windll.user32.MessageBoxW(0, f'The file "{pdfname}.pdf" probably has a space in its name: "name .pdf".\nPlease remove this space.\nError in os.Listdir', "Error", 1)            
+        try:
+            if os.listdir(target_dir) == []:
+                #The method convert_from_path will work for a lot of pdfs                 #
+                #Yet when this method fails convert_from_bytes might convert pdfs that    #
+                #couldn't be converted with from_path, if both methods don't work the user#
+                #is informed to use an online-converter instead.                          #
+                pages = convert_from_path(item, 170)
+                if pages == []:
+                    pages = convert_from_bytes(open(origin_dir, 'rb').read())
+                    
+                if pages != []:
+                    ctypes.windll.user32.MessageBoxW(0, f'The file "{pdfname}.pdf" could not be converted to jpg.\nPlease use an online pdf converter instead.\nThen place the jpg files in a map folder named after the book in {self.dir3}', "Warning", 1)            
+                    
+                if pages != []:
+                    i = 1
+                    for page in pages:
+                        filename = os.path.join(target_dir, pdfname)
+                        nr = "0"*(4-len(f"{i}"))+f"{i}"
+                        page.save(f'{filename}{nr}.jpg', 'JPEG')
+                        i += 1
+        except:
+            pass
