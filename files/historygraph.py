fd61a8f8fb50048f76467a6c9cdbb1152ee9e129
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-03-12 12:23:50 +0100

    Update historygraph.py

--- files/historygraph.py
+++ files/historygraph.py
@@ -21,7 +21,7 @@ import numpy as np
 import json
 import time
 from datetime import datetime
-
+import random
 
 
    
@@ -63,6 +63,7 @@ def takeSecond(elem):
     return elem[1]
 
 def textcard(TEXT,width,textpos):
+    
     height_card = 0.5
     fig = Figure(figsize=[width, height_card],dpi=100)
     fig.patch.set_facecolor((254/255,240/255,231/255,1))
@@ -81,6 +82,7 @@ def textcard(TEXT,width,textpos):
     return PIL.Image.frombytes("RGB", size, raw_data, decoder_name='raw', )
 
 def drawcard(X,Y,legend2):
+    Y = Sec2Min(Y)
     legend = [legend2[x] for x in X]
     print(X)
     X_it,Y_it,legend_it = iter(X),iter(Y),iter(legend)
@@ -94,7 +96,8 @@ def drawcard(X,Y,legend2):
     print(X)
     
     height_card = 2
-    fig = Figure(figsize=[4, height_card],dpi=100)
+    width_card = 3
+    fig = Figure(figsize=[width_card, height_card],dpi=100)
     fig.patch.set_facecolor((254/255,240/255,231/255,1)) #flashbook theme color rescaled to [0,1]
     ax = fig.gca()
     for i,x in enumerate(X):
@@ -116,20 +119,21 @@ def drawlegend(totalbooks,totalvalues,legendbackup,hatchlist):
     #f = lambda m,c: plt.plot([],[],marker=m, color=c, ls="none")[0]
     hatch_it = iter(hatchlist)
     handles = [mpatches.Patch(edgecolor='black',facecolor=c,hatch=next(hatch_it),label="hallo") for c in colors]
-    
+    import textwrap
     tv_it = iter(totalvalues)
     labels = totalbooks
-    labels = [f"{x} -- {next(tv_it)}" for x in totalbooks]
+    labels = [textwrap.fill(f"{x} ({ round(next(tv_it),1)} minutes)",40) for x in totalbooks]
     legend = plt.legend(handles, labels, loc=2, framealpha=False, frameon=True,markerscale=3.6,markerfirst = True,fontsize=15)
     #expand=[-5,-5,2,2]
-    fig  = Figure(figsize=[4, 0],dpi=100)
+    fig  = Figure(figsize=[4, 40],dpi=100)
     fig  = legend.figure
+    fig.set_size_inches(8, 8)
     fig.canvas.draw()
     ax = fig.gca()
     ax.axis('off')
     ax.get_xaxis().set_visible(False)
     fig.patch.set_facecolor((254/255,240/255,231/255,1))
-    fig.show()
+    #fig.show()
     canvas = FigureCanvas(fig)
     canvas.draw()
     renderer = canvas.get_renderer()
@@ -156,16 +160,23 @@ def sortsubdata(data):
     X2.sort(key=takeSecond)
     return X2
 
+def Sec2Min(listing):   
+    return [round(item/60,1) for item in listing]
 
 def CreateGraph(self):
     datethreshold = self.GraphNdays
-    
-    with open(os.path.join(self.dir4,"timecount_flashbook.json"), 'r') as file:
-        timedict_fb = json.load(file)
-    file.close()
-    with open(os.path.join(self.dir4,"timecount_flashcard.json"), 'r') as file2:
-        timedict_fc = json.load(file2)
-    file2.close()
+    try:
+        with open(os.path.join(self.dir4,"timecount_flashbook.json"), 'r') as file:
+            timedict_fb = json.load(file)
+        file.close()
+    except:
+        timedict_fb = {}
+    try:
+        with open(os.path.join(self.dir4,"timecount_flashcard.json"), 'r') as file2:
+            timedict_fc = json.load(file2)
+        file2.close()
+    except:
+        timedict_fc = {}
     
     #% COLLECT ALL DATA
     data_fb = GetValues(timedict_fb,datethreshold)
@@ -179,50 +190,57 @@ def CreateGraph(self):
     for i,x in enumerate(X):
         if x[0] not in totalbooks:
             totalbooks.append(x[0])
-            totalvalues.append(x[1])     
-    
-    #variable n should be number of curves to plot (I skipped this earlier thinking that it is obvious when looking at picture - sorry my bad mistake xD): n=len(array_of_curves_to_plot)
-    color = cm.nipy_spectral(np.linspace(0,1,len(totalbooks)))    
-    
-    #CREATE LEGEND: colors and hatches (pattern displayed on barplots)
-    hatch_it = iter(["","/////","---","\\\\\\\\\\"]*len(totalbooks))  
-    hatchlist = []
-    for _ in enumerate(totalbooks):
-        hatchlist.append(next(hatch_it))
-    
-    legend = {}
-    for i,book in enumerate(totalbooks):   
-        legend[book] = tuple((color[i],hatchlist[i] ))
-    
-    legendbackup = legend    
-    
-    
-    #CREATE TEXT IMAGES
-    txt1 = textcard("Today",4,4)
-    txt2 = textcard(f"Last {datethreshold} days", 4,4)
-    txt3 = textcard("Flashbook",2,4).rotate(90, expand = 1)
-    txt4 = textcard("Flashcard",2,4).rotate(90, expand = 1)
-    
-    #CREATE IMAGES
-    im1 = drawcard(data_fb[2],data_fb[3],legend)
-    im2 = drawcard(data_fb[0],data_fb[1],legend)
-    im3 = drawcard(data_fc[2],data_fc[3],legend)
-    im4 = drawcard(data_fc[0],data_fc[1],legend)
-    im5 = drawlegend(totalbooks,totalvalues,legendbackup,hatchlist)
-    
-    # COMBINE ALL IMAGES TO A MOZAIC
-    width = im1.width + im2.width + txt3.width+im5.width
-    height = im1.height*2 + txt1.height
-    new_im = PIL.Image.new('RGB', (width, height), (254,240,231))
-    new_im.paste(txt1, (txt3.width,0))
-    new_im.paste(txt3, (0,txt1.height))
-    new_im.paste(txt4, (0,txt1.height+txt3.height))
-    new_im.paste(txt2, (txt3.width + txt1.width,0))
-    new_im.paste(im1, (txt3.width,txt1.height))
-    new_im.paste(im2, (txt3.width+im1.width,txt2.height))
-    new_im.paste(im3, (txt3.width,txt1.height+im1.height))
-    new_im.paste(im4, (txt3.width+im1.width ,txt2.height+im2.height))
-    new_im.paste(im5, (txt3.width+im1.width+im2.width ,txt2.height))
-    new_im.paste
-    return new_im
+            totalvalues.append(x[1]/60)     
+    if len(totalvalues) > 0:
+        #variable n should be number of curves to plot (I skipped this earlier thinking that it is obvious when looking at picture - sorry my bad mistake xD): n=len(array_of_curves_to_plot)
+        
+        lvlC = np.linspace(0.03,0.97,len(totalbooks))
+        #random.shuffle(lvlC)
+        color = cm.Paired(lvlC)
+        color = cm.nipy_spectral(lvlC)           
+        
+        #CREATE LEGEND: colors and hatches (pattern displayed on barplots)
+        hatch_it = iter(["","/////","---","\\\\\\\\\\"]*len(totalbooks))  
+        hatchlist = []
+        for _ in enumerate(totalbooks):
+            hatchlist.append(next(hatch_it))
+        
+        legend = {}
+        for i,book in enumerate(totalbooks):   
+            legend[book] = tuple((color[i],hatchlist[i] ))
+        
+        legendbackup = legend    
+        
+        
+        #CREATE TEXT IMAGES
+        txt1 = textcard("Today",3.5,2)
+        txt2 = textcard(f"Last {datethreshold} days", 3,2)
+        txt3 = textcard("Flashbook",2,4).rotate(90, expand = 1)
+        txt4 = textcard("Flashcard",2,4).rotate(90, expand = 1)
+        
+        #CREATE IMAGES
+        im1 = drawcard(data_fb[2],data_fb[3],legend)
+        im2 = drawcard(data_fb[0],data_fb[1],legend)
+        im3 = drawcard(data_fc[2],data_fc[3],legend)
+        im4 = drawcard(data_fc[0],data_fc[1],legend)
+        im5 = drawlegend(totalbooks,totalvalues,legendbackup,hatchlist)
+        
+        # COMBINE ALL IMAGES TO A MOZAIC
+        width = im1.width + im2.width + txt3.width+im5.width
+        height = im1.height*2 + txt1.height
+        new_im = PIL.Image.new('RGB', (width, height), (254,240,231))
+        new_im.paste(txt1, (txt3.width,0))
+        new_im.paste(txt3, (0,txt1.height))
+        new_im.paste(txt4, (0,txt1.height+txt3.height))
+        new_im.paste(txt2, (txt3.width + txt1.width,0))
+        new_im.paste(im1, (txt3.width,txt1.height))
+        new_im.paste(im2, (txt3.width+im1.width,txt2.height))
+        new_im.paste(im3, (txt3.width,txt1.height+im1.height))
+        new_im.paste(im4, (txt3.width+im1.width ,txt2.height+im2.height))
+        new_im.paste(im5, (txt3.width+im1.width+im2.width ,txt2.height))
+        new_im.paste
+    else:
+        new_im = PIL.Image.new('RGB', (0, 0), (254,240,231))
+    BOOL = len(totalvalues) > 0
+    return BOOL, new_im
     
