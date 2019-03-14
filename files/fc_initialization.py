fc9eafa87566e95fd3f7fb6d5054a507ffd627a7
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-03-14 10:17:39 +0100

    Update fc_initialization.py

--- files/fc_initialization.py
+++ files/fc_initialization.py
@@ -37,22 +37,4 @@ def initializeparameters(self):
     self.questions2  = []
     f2.SetScrollbars_fc(self)
     
-    # set mouse short cuts: 
-    self.m_bitmapScroll.Bind( wx.EVT_MOUSEWHEEL, self.m_toolSwitch21OnToolClicked )
-    self.m_bitmapScroll.Bind( wx.EVT_LEFT_DOWN, self.m_buttonCorrectOnButtonClick)
-    self.m_bitmapScroll.Bind( wx.EVT_RIGHT_DOWN, self.m_buttonWrongOnButtonClick )        
     
-    # set keyboard short cuts: accelerator table        
-    Id_Correct = wx.NewIdRef()
-    Id_Wrong   = wx.NewIdRef() 
-    Id_card    = wx.NewIdRef() 
-    # combine functions with the id
-    self.Bind( wx.EVT_MENU, self.m_buttonCorrectOnButtonClick, id = Id_Correct )
-    self.Bind( wx.EVT_MENU, self.m_buttonWrongOnButtonClick,   id = Id_Wrong   )
-    self.Bind( wx.EVT_MENU, self.m_toolSwitch21OnToolClicked,    id = Id_card    )
-    # combine id with keyboard = now keyboard is connected to functions
-    entries = wx.AcceleratorTable([(wx.ACCEL_NORMAL,  wx.WXK_LEFT, Id_Correct),
-                                  (wx.ACCEL_NORMAL,  wx.WXK_RIGHT, Id_Wrong ),
-                                  (wx.ACCEL_NORMAL,  wx.WXK_UP, Id_card),
-                                  (wx.ACCEL_NORMAL,  wx.WXK_DOWN, Id_card)])
-    self.SetAcceleratorTable(entries)
