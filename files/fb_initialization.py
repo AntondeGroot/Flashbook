cde7e427d8e5d6015ec77413a10450ef6cce9783
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-03-20 23:55:07 +0100

    Update fb_initialization.py

--- files/fb_initialization.py
+++ files/fb_initialization.py
@@ -11,7 +11,7 @@ def initializeparameters(self):
     self.m_bitmapScroll.Bind( wx.EVT_RIGHT_DOWN, self.m_resetselectionOnButtonClick )
     self.m_bitmapScroll.Bind( wx.EVT_MIDDLE_DOWN, self.m_enterselectionOnButtonClick )
     # for scrolling: only remember current and last position, append and pop, if the numbers repeat [0,0] or [X,X] then you know you've reached either the beginning or the end of the window: then flip page 
-    self.m_dirPicker11.SetInitialDirectory(self.dir3) #set initial directory
+    self.m_dirPicker11.SetInitialDirectory(str(self.booksdir)) #set initial directory
     # initialize variables:
     self.bookname       = ''
     self.BorderCoords   = []         
