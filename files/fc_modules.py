610016ed2ff13db1b69d6512c90afb038aacfe6e
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-04-06 11:48:57 +0200

    Update fc_modules.py

--- files/fc_modules.py
+++ files/fc_modules.py
@@ -50,7 +50,12 @@ def buttonCorrect(self):
             self.index = (self.nr_questions-1)
             f2.remove_stats(self)
             _score_ = round(float(self.score)/self.nr_questions*100,1)
-            MessageBox(0, f"Your score is: {_score_}%", "Result", 1 )     
+            #MessageBox(0, f"Your score is: {_score_}%", "Result", 1 )    
+            message = f"Your score is: {_score_}%"
+            with gui.MyDialogScore(self,message) as dlg:
+                if dlg.ShowModal() == wx.ID_OK:    
+                    print("finished")
+            self.m_menubar1.EnableTop(2,False)
             runprogram = False
             if hasattr(self,'bookname'): # to stop from pop-up windows from appearing after the test is done
                 delattr(self,'bookname')
@@ -89,7 +94,12 @@ def buttonWrong(self):
             self.index = (self.nr_questions-1)
             f2.remove_stats(self)
             _score_ = round(float(self.score)/self.nr_questions*100,1)
-            MessageBox(0, f"Your score is: {_score_}%", "Result", 1)     
+            #MessageBox(0, f"Your score is: {_score_}%", "Result", 1) 
+            message = f"Your score is: {_score_}%"
+            with gui.MyDialogScore(self,message) as dlg:
+                if dlg.ShowModal() == wx.ID_OK:    
+                    print("finished")
+            self.m_menubar1.EnableTop(2,False)
             runprogram = False
             if hasattr(self,'bookname'): # to stop from pop-up windows from appearing after the test is done
                 delattr(self,'bookname')
@@ -117,59 +127,110 @@ def buttonWrong(self):
             id_ = self.m_toolSwitch21.GetId()
             self.m_toolBar3.SetToolNormalBitmap(id_, wx.Bitmap( str(path_repeat), wx.BITMAP_TYPE_ANY ))
         self.runprogram = runprogram
+
+def buttonPreviousCard(self):
+    f2.clearbitmap(self)
+    if hasattr(self,'nr_questions') and self.nr_questions != 0 and hasattr(self,'bookname') and self.bookname != '':
+        runprogram = self.runprogram
+        if self.index != 1:
+            self.index -= 1
+            self.score -= 1
+        self.mode = 'Question'
+        self.m_textCtrlMode.SetValue(self.mode)  
+        if self.index > (self.nr_questions-1):
+            self.index = (self.nr_questions-1)
+            f2.remove_stats(self)
+            _score_ = round(float(self.score)/self.nr_questions*100,1)
+            #MessageBox(0, f"Your score is: {_score_}%", "Result", 1) 
+            message = f"Your score is: {_score_}%"
+            with gui.MyDialogScore(self,message) as dlg:
+                if dlg.ShowModal() == wx.ID_OK:    
+                    print("finished")
+            self.m_menubar1.EnableTop(2,False)
+            runprogram = False
+            if hasattr(self,'bookname'): # to stop from pop-up windows from appearing after the test is done
+                delattr(self,'bookname')
+            
+        if self.score > self.nr_questions+1:
+            self.score = self.nr_questions
+            _score_ = round(float(self.score)/self.nr_questions*100,1)
+        _score_ = round(float(self.score)/self.nr_questions*100,1)
+        self.m_Score21.SetValue(f"{_score_} %")      
+        self.m_CurrentPage21.SetValue(str(self.index+1))
+        
+        ## update stats
+        if runprogram == True:
+            f2.set_stats(self)
+            f2.save_stats(self)    
+            f2.displaycard(self)
+            f2.switch_bitmap(self)
+        f2.SetScrollbars_fc(self)
+        if runprogram == False:
+            self.m_Score21.SetValue("")     
+            self.m_CurrentPage21.SetValue("")
+            self.m_TotalPages21.SetValue("")
+            #reset pictogram
+            path_repeat    = Path(self.resourcedir,"repeat.png")
+            id_ = self.m_toolSwitch21.GetId()
+            self.m_toolBar3.SetToolNormalBitmap(id_, wx.Bitmap( str(path_repeat), wx.BITMAP_TYPE_ANY ))
+        self.runprogram = runprogram
+
         
 def switchCard(self):
     #matplotlib.pyplot.close('all') # otherwise too many pyplot figures will be opened -> memory
     f2.clearbitmap(self)
-    if self.runprogram == True:
-        # change mode Q <-> A
-        if self.mode == 'Question': 
-            self.mode = 'Answer'
-        else:
-            self.mode = 'Question'
-        self.m_textCtrlMode.SetValue(self.mode)
-        # check if there is an answer: if not switch_bitmap sets the mode back to 'question'
-        f2.switch_bitmap(self) 
-        self.TextCard = False
-        AbsoluteIndex = self.cardorder[self.index] 
-        self.key = f'{self.mode[0]}{AbsoluteIndex}' #e.g. Q12 is a key
-        if self.debugmode:
-            print(f"current key = {self.key}")
-        # if there are no answer cards, then don't switch card: the self.key makes sure this happens
-        if f'A{AbsoluteIndex}' not in self.textdictionary:
+    try:
+        if self.runprogram == True:
+            # change mode Q <-> A
+            if self.mode == 'Question': 
+                self.mode = 'Answer'
+            else:
+                self.mode = 'Question'
             self.m_textCtrlMode.SetValue(self.mode)
-            self.key = f'{self.mode[0]}{AbsoluteIndex}'
-        # if there are answerd cards, switch
-        if self.key in self.textdictionary:
-            try:
-                f2.CreateTextCard(self)
-            except:
-                log.ERRORMESSAGE("Error: failed to create TextCard")
-        #There is text, determine if there is a picture:
-        if self.TextCard == True:
+            # check if there is an answer: if not switch_bitmap sets the mode back to 'question'
+            f2.switch_bitmap(self) 
+            self.TextCard = False
+            AbsoluteIndex = self.cardorder[self.index] 
+            self.key = f'{self.mode[0]}{AbsoluteIndex}' #e.g. Q12 is a key
             if self.debugmode:
-                print(f"\n\nwe have key {self.key} for picdictionary {self.picdictionary}\n\n")
-            if self.key in self.picdictionary:
-                try: # to combine text with picture
-                    key = self.key
-                    imagetext = self.imagetext
-                    self.image = f2.CombinePicText_fc(self,key,imagetext)
-                    f2.ShowPage_fc(self)     
-                except:
-                    log.ERRORMESSAGE("Error: cannot combine pic with text")
-            else: #only display text
-                self.image = self.imagetext
-                f2.ShowPage_fc(self)
-        # there is no text: but is there a picture?
-        else: #only display picture
-            if self.key in self.picdictionary: # there is a picture
+                print(f"current key = {self.key}")
+            # if there are no answer cards, then don't switch card: the self.key makes sure this happens
+            if f'A{AbsoluteIndex}' not in self.textdictionary:
+                self.m_textCtrlMode.SetValue(self.mode)
+                self.key = f'{self.mode[0]}{AbsoluteIndex}'
+            # if there are answerd cards, switch
+            if self.key in self.textdictionary:
                 try:
-                    self.image = f2.findpicture(self,self.key)
-                    f2.ShowPage_fc(self)
+                    f2.CreateTextCard(self,self.key,'flashcard')
                 except:
-                    pass
-            # you don't need to check for: "no Text & no picture" because switch_bitmap already takes care of that.
-        f2.SetScrollbars_fc(self)
+                    log.ERRORMESSAGE("Error: failed to create TextCard")
+            #There is text, determine if there is a picture:
+            if self.TextCard == True:
+                if self.debugmode:
+                    print(f"\n\nwe have key {self.key} for picdictionary {self.picdictionary}\n\n")
+                if self.key in self.picdictionary:
+                    try: # to combine text with picture
+                        key = self.key
+                        imagetext = self.imagetext
+                        self.image = f2.CombinePicText_fc(self,key,imagetext)
+                        f2.ShowPage_fc(self)     
+                    except:
+                        log.ERRORMESSAGE("Error: cannot combine pic with text")
+                else: #only display text
+                    self.image = self.imagetext
+                    f2.ShowPage_fc(self)
+            # there is no text: but is there a picture?
+            else: #only display picture
+                if self.key in self.picdictionary: # there is a picture
+                    try:
+                        self.image = f2.findpicture(self,self.key)
+                        f2.ShowPage_fc(self)
+                    except:
+                        pass
+                # you don't need to check for: "no Text & no picture" because switch_bitmap already takes care of that.
+            f2.SetScrollbars_fc(self)
+    except:
+        pass
 
 def startprogram(self,event): 
     """main program that does all the preprocessing"""
@@ -224,6 +285,8 @@ def startprogram(self,event):
     and use these values to set the slider as you wish. Don't forget to add "self.Destroy" when you press the button"""
     
     data = self.nr_cards
+    if data == 1:
+        data = 2
     try:
         with open(self.statsdir, 'r') as file:    
             dictionary = json.load(file)
@@ -280,3 +343,5 @@ def startprogram(self,event):
     f2.LoadFlashCards(self, True,letterfile)
     f2.displaycard(self)        
     f2.switch_bitmap(self)
+
+
