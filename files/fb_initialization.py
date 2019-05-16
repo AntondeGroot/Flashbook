c65d9f16b4edd8bdc075218ca2f577b44dc6d0e2
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-05-17 00:04:03 +0200

    Update fb_initialization.py

--- files/fb_initialization.py
+++ files/fb_initialization.py
@@ -11,7 +11,7 @@ def initializeparameters(self):
     self.m_bitmapScroll.Bind( wx.EVT_RIGHT_DOWN, self.m_resetselectionOnButtonClick )
     self.m_bitmapScroll.Bind( wx.EVT_MIDDLE_DOWN, self.m_enterselectionOnButtonClick )
     # for scrolling: only remember current and last position, append and pop, if the numbers repeat [0,0] or [X,X] then you know you've reached either the beginning or the end of the window: then flip page 
-    self.m_dirPicker11.SetInitialDirectory(str(self.booksdir)) #set initial directory
+    self.m_dirPickerFB.SetInitialDirectory(str(self.booksdir)) #set initial directory
     # initialize variables:
     self.bookname       = ''
     self.BorderCoords   = []         
@@ -23,7 +23,7 @@ def initializeparameters(self):
     self.panel_pos      = (0,0)        
     self.questionmode   = True
     self.zoom           = 1.0
-    self.m_textCtrl1.SetValue("Question:")
-    self.m_Zoom11.SetValue(f"{int(self.zoom*100)}%")  
+    self.m_modeDisplay.SetValue("Question:")
+    self.m_ZoomFB.SetValue(f"{int(self.zoom*100)}%")  
     #f.ResetQuestions(self)
     f.SetScrollbars(self)
