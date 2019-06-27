9cccbee5f58f4db1ee35b5b19617c846b8e2ad1f
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-06-28 00:30:54 +0200

    Update fc_functions.py

--- files/fc_functions.py
+++ files/fc_functions.py
@@ -4,6 +4,7 @@ Created on Fri Sep 14 12:39:47 2018
 
 @author: Anton
 """
+import latexoperations as ltx
 import json
 import math
 import os
@@ -40,6 +41,51 @@ MB_DEFBUTTON2 = 0x00000100
 #   find_arguments(5)         - finds all the arguments for a certain command
 #   replace_allcommands(4)    - replaces user defined commands into LaTeX commands that are known.
 """
+
+def findpicture_path(self,picname):
+    """Instead of just opening the path of a picture
+    Try to find out if the path exists
+    if it does not exist, try to look in all other folders.
+    This problem may occur if you have combined several books.
+    If the picture really doesn't exist, then the user gets notified with a messagebox."""
+    FOUNDPIC = False
+    imagepic = None
+    #if key in self.picdictionary:
+    
+    path = Path(self.picsdir, self.bookname, picname)
+    if path.exists():
+        imagepic = PIL.Image.open(str(path))
+        FOUNDPIC = True
+    else:
+        folders = os.listdir(self.picsdir)
+        for i,item in enumerate(folders):
+            path = Path(self.picsdir, item, picname)
+            if path.exists():
+                imagepic = PIL.Image.open(str(path))
+                FOUNDPIC = True
+    if FOUNDPIC == False:
+        """Notify User and create a fake picture with the error message 
+        as replacement for the missing picture."""
+        
+        MessageBox(0, f"Error in line : {picname}\nPicture could not be found in any folder.", "Message", ICON_STOP)
+        LaTeXcode =  "This image does not exist"
+        height_card = math.ceil(len(LaTeXcode)/40)/2
+        fig = Figure(figsize=[8, height_card],dpi=100)
+        ax = fig.gca()
+        ax.plot([0, 0,0, height_card],color = (1,1,1,1))
+        ax.axis('off')
+        ax.text(-0.5, height_card/2,LaTeXcode, fontsize = self.LaTeXfontsize, horizontalalignment='left', verticalalignment='center',wrap = True,color = 'r')    
+        canvas = FigureCanvas(fig)
+        canvas.draw()        
+        renderer = canvas.get_renderer()
+        raw_data = renderer.tostring_rgb()
+        size = canvas.get_width_height()
+        # output
+        imagepic = PIL.Image.frombytes("RGB", size, raw_data, decoder_name = 'raw', )
+        FOUNDPIC = True #the user should be informed and shown that the picture does not exist, because it should exist!
+
+    return FOUNDPIC, imagepic
+
 
 def findpicture(self,key):
     """Instead of just opening the path of a picture
@@ -461,8 +507,14 @@ def displaycard(self):
     #try:
     key = f'card_{self.mode[0].lower()}{self.cardorder[self.index]}'
     # try to create a TextCard
-    bool_textcard, img_txt = CreateTextCard(self,'flashcard',key)
-    bool_piccard, img_pic  = findpicture(self,key)
+    line = self.CardsDeck.getcards()[key]['text']
+    txt = ltx.argument(r"\\text{",line)
+    pic = ltx.argument(r"\\pic{",line)
+    print(f"\nANTON {self.CardsDeck.getcards()[key].keys()}"*10)
+    print(f"text = {txt}")
+    
+    bool_textcard, img_txt = CreateTextCard(self,'manual',txt)    
+    bool_piccard, img_pic  = findpicture_path(self,pic)
     image = CombinePicText_fc(bool_textcard,img_txt,bool_piccard,img_pic)
     ShowPage_fc(self,image) 
     
@@ -489,6 +541,7 @@ def CreateSingularCard(self,mode):
     try:
         key = f'card_{self.mode[0].lower()}{self.cardorder[self.index]}'
         # try to create a TextCard
+        
         bool_textcard, img_text = CreateTextCard(self,'flashcard',key)
         bool_piccard,  img_pic  = findpicture(self,key)
         image = CombinePicText_fc(bool_textcard,img_text,bool_piccard,img_pic)
