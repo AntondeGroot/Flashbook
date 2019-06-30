2202e1dbcd7ee10ed32e451bd7d7277856c25dd0
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-07-01 01:34:57 +0200

    Update class_flashcard.py

--- files/class_flashcard.py
+++ files/class_flashcard.py
@@ -16,7 +16,7 @@ from latexoperations import Commands as cmd
 import log_module    as log
 ICON_EXCLAIM=0x30
 import accelerators_module as m7
-
+import os
 import ctypes
 ICON_STOP = 0x10
 MB_ICONINFORMATION = 0x00000040
@@ -33,10 +33,40 @@ class flashcard(gui.MyFrame):
         pass
     def m_OpenFlashcardOnButtonClick( self, event ):
         """START MAIN PROGRAM : FLASCARD"""
+        # set all directories
+        os.chdir(self.notesdir)                
+        dirs = [self.appdir,self.notesdir,self.picsdir,self.booksdir,self.tempdir,self.bordersdir,self.resourcedir]        
+        print("="*90)
+        print(f"\nThe files will be saved to the following directory: {self.appdir}\n")     
+        for dir_ in dirs:
+            if not dir_.exists():
+                dir_.mkdir()
+                    
+        # initialize variables:
+        self.bookname       = ''
+        self.image          = []
+        self.panel_pos      = (0,0)        
+        self.zoom           = 1.0
+        self.m_ZoomFC.SetValue(f"{int(self.zoom*100)}%")  
+        #
+        self.runprogram = True
+        self.SwitchCard = True
+        self.index = 0
+        self.score = 0
+        self.nr_questions = 0
+        self.mode = 'Question'
+        self.m_modeDisplayFC.SetValue(self.mode)
+        self.cardorder   = []        
+        self.questions   = []
+        self.answers     = []
+        self.questions2  = []
+        f2.SetScrollbars_fc(self)
+        
+        
         self.NEWCARD = True
         m7.AcceleratorTableSetup(self,"flashcard","set")
         p.SwitchPanel(self,2)
-        p.run_flashcard(self)
+        #p.run_flashcard(self)
         with wx.FileDialog(self, "Choose a subject to study",defaultDir=str(self.notesdir), wildcard="*.tex",style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 #the user changed their mind
