a7a6a3ab5882abe269f32f9ac97caef45aff925b
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-07-04 12:34:42 +0200

    Update class_menuflashcard.py

--- files/class_menuflashcard.py
+++ files/class_menuflashcard.py
@@ -6,33 +6,15 @@ Created on Fri Jun 28 14:04:23 2019
 """
 
 import gui_flashbook as gui
-import threading
 import wx
-import PIL
-import program as p
-import fc_functions    as f2
-import print_modules as m3
-import fc_modules as m2
-from pathlib import Path
-from latexoperations import Commands as cmd
+import fc_functions as f2
+import fc_modules   as m2
 import latexoperations as ltx
-import imageoperations as imop
-import log_module    as log
-ICON_EXCLAIM=0x30
-
-import ctypes
-ICON_STOP = 0x10
-MB_ICONINFORMATION = 0x00000040
-MessageBox = ctypes.windll.user32.MessageBoxW
-MB_YESNO = 0x00000004
-MB_DEFBUTTON2 = 0x00000100
 
 class flashcardmenu(gui.MyFrame):
     def __init__(self):
         pass
-    def m_menuAddCardOnMenuSelection( self, event ):
-        print("b cardorder")
-        print(self.cardorder)   
+    def m_menuAddCardOnMenuSelection( self, event ):  
         trueindex = self.cardorder[self.index]
         data = ['Add a card', '' , '' , '' ]
         
@@ -50,8 +32,7 @@ class flashcardmenu(gui.MyFrame):
                     self.m_TotalCards.SetValue(f"{self.nr_questions}")
                     self.CardsDeck.reset()
                     self.Latexfile.loadfile(self.booknamepath)
-                    cards = self.Latexfile.file_to_rawcards()#cards contains q,a,t,s
-                    print(f"cards = {cards}")
+                    cards = self.Latexfile.file_to_rawcards() #cards contains q,a,t,s
                     self.CardsDeck.set_cards(cards=cards,notesdir=self.notesdir)
                     f2.switch_bitmap(self)
                     f2.displaycard(self)
@@ -65,4 +46,3 @@ class flashcardmenu(gui.MyFrame):
            
     def m_menuPreviousCardOnMenuSelection( self, event ):
         m2.buttonPreviousCard(self)
-        print("go to previous card")
