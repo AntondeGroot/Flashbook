a25260f470be9029aa4ddf3aa231956ef5edd886
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-07-09 12:39:51 +0200

    Add files via upload

---
+++ files/class_progressdialog.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 11:47:03 2019
+
+@author: aammd
+"""
+import wx
+class ProgressDialog():
+    def __init__(self,max=100,title='',message=''):
+        self.dialog = wx.ProgressDialog(title,
+                                        message,
+                                        maximum=100,
+                                        parent=None,
+                                        style = wx.PD_CAN_ABORT
+                                        #| wx.PD_APP_MODAL
+                                        | wx.PD_AUTO_HIDE
+                                        #| wx.PD_ELAPSED_TIME
+                                        #| wx.PD_ESTIMATED_TIME
+                                        #| wx.PD_REMAINING_TIME
+                                        )
+    def SetRange(self,integer):
+        self.dialog.SetRange(integer)
+        
+    def Pulse(self, msg=''):
+        self.dialog.Pulse(newmsg=msg)
+        
+    def Update(self,index, msg):
+        (keepGoing, skip) = self.dialog.Update(index, msg)
+        return (keepGoing, skip)
+        
+    def Destroy(self):
+        """ The progress dialog cannot be destroyed by dlg.Destroy() as some sources on the internet
+        may suggest. Instead this will make sure it will reach it maximum value which will make sure
+        it closes anyway."""
+        try:
+            maxrange = self.dialog.GetRange()
+            self.dialog.Update(maxrange)
+        except:
+            pass
+    
