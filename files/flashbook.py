ea8c681df0852a6d66f80189798f3440688904e3
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-10-03 14:37:06 +0200

    Update flashbook.py

--- files/flashbook.py
+++ files/flashbook.py
@@ -121,13 +121,13 @@ class MainFrame(settings,flashbook,flashcard,printer,filetransfer,menusettings,h
         
         self.Latexfile = Latexfile()
         self.Flashcard = Flashcard(self.LaTeXfontsize)
-        self.CardsDeck = CardsDeck()#Flashcard(self.LaTeXfontsize))
+        self.CardsDeck = CardsDeck()
         
         self.library   = [None]
         
-        self.m_menubar1.EnableTop(2, False)#disable Flashcard menu
+        self.m_menubar1.EnableTop(2, False) # disable Flashcard menu
         self.Maximize(True) # open the app window maximized
-        t_books = lambda self,delay : threading.Thread(target = p.checkBooks , args=(self,delay )).start()
+        t_books = lambda self,delay : threading.Thread(target = p.checkBooks , args=(self, delay)).start()
         t_books(self, 0.1) 
         
         self.stitchmode_v = True
