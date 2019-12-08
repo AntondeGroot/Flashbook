d3386409b3d018967af085fc7b121320c4dda82c
AntondeGroot <39248332+AntondeGroot@users.noreply.github.com>
2019-12-08 18:32:41 +0100

    Update class_filetransfer.py

--- files/class_filetransfer.py
+++ files/class_filetransfer.py
@@ -31,7 +31,7 @@ def initialize(self):
             myIP = wmi_out.IPAddress[0]          
            
             with open(Path(self.dirIP,'IPadresses.txt'),'w') as f:
-                f.write(json.dumps({'IP1' : myIP,'IP2': "",'client':True})) 
+                f.write(json.dumps({'IP1' : myIP,'IP2': ""})) 
                 f.close()
     except:
         log.ERRORMESSAGE("CLASS FILETRANSFER: Error: could not access internet")
@@ -61,25 +61,23 @@ class filetransfer(gui.MyFrame):
             f.close()
     
     def m_buttonTransferOnButtonClick(self,event):
+        
+        """First try to run the Client, if the server is online it proceeds normally. 
+        When no server is detected it will assume the role of server.
+        This way the user does not have to choose which side is client/server."""
+        
         log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: starting synchronisation')
-        Client = True
-        if Client: 
-            """First try to run the Client, if the server is online it proceeds normally. 
-            When no server is detected it will assume the role of server.
-            This way the user does not have to choose which side is client/server."""
-            HOST = self.IP2
-            
-            self.SetCursor(wx.Cursor(wx.CURSOR_ARROWWAIT))
-            SERVERONLINE = f4.CheckServerStatus(HOST,65432)
-            self.SetCursor(wx.Cursor(wx.CURSOR_ARROW))
-            log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: Host is {HOST}\n\t Starting Client\n\t server is online: {SERVERONLINE}')
-            if SERVERONLINE:
-                self.m_txtStatus.SetValue("starting client")
-                t_sync = lambda self,mode,HOST :threading.Thread(target=m4.SyncDevices,args=(self,mode,HOST)).start()
-                t_sync(self,"CLIENT",HOST)        
-            else:
-                HOST = self.IP1
-                log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: Host is {HOST}\n\t Starting Server')
-                self.m_txtStatus.SetValue("starting server")
-                t_sync = lambda self,mode,HOST :threading.Thread(target=m4.SyncDevices,args=(self,mode,HOST)).start()
-                t_sync(self,"SERVER",HOST)
+        HOST = self.IP2
+        self.PORT = 65432 # Port to listen on (non-privileged ports are > 1023)
+        SERVERONLINE = f4.CheckServerStatus(HOST,self.PORT)
+        log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: \n\t server is online: {SERVERONLINE}')
+        if SERVERONLINE:
+            self.m_txtStatus.SetValue("starting client")
+            log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: \n\t Starting Client')
+            t_sync = lambda self, :threading.Thread(target=m4.Thread_Client,args=(self,)).start()
+            t_sync(self)        
+        else:
+            self.m_txtStatus.SetValue("starting server")
+            log.DEBUGLOG(debugmode=self.debugmode,msg=f'CLASS FILETRANSFER: \n\t Starting Server')
+            t_sync = lambda self, :threading.Thread(target=m4.Thread_Server,args=(self,)).start()
+            t_sync(self)
